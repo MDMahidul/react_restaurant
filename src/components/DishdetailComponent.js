6eb938fc30e431af7d3b93be90052c232a445653
MD. Mahidul Islam <mmbmahidul007@gmail.com>
2020-11-02 13:15:44 +0600

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,8 +17,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         // event.preventDefault();
     }
 
@@ -117,7 +117,7 @@ function RenderDish({dish}) {
         }
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if(comments != null) {
             return (
                 <div className="col-12 col-md-5 m-1">
@@ -132,7 +132,7 @@ function RenderDish({dish}) {
                                 );
                             })}
                         </ul>
-                        <CommentForm />
+                        <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
         } else {
@@ -159,7 +159,10 @@ function RenderDish({dish}) {
                 </div>
                     <div className="row">
                         <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+                        />
                     </div>
                 </div>
             );
